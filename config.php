083358a3a6fcd03f1f3552c02bf266c0075ca996
László Vaszi <vaszi92@gmail.com>
2017-02-07 12:56:55 +0100

    Update config.php

--- config.php
+++ config.php
@@ -4,6 +4,6 @@
 defined('DB_SERVER') ? null : define("DB_SERVER", "127.0.0.1");
 defined('DB_USER') ? null : define("DB_USER", "root");
 defined('DB_PASS') ? null : define("DB_PASS", "root");
-defined('DB_NAME') ? null : define("DB_NAME", "kulcs_soft");
+defined('DB_NAME') ? null : define("DB_NAME", "db_name");
 	
 ?>
